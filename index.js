f2bc4ad25995459ad21b82cdc70ed8a2a77e335f
vega-create <vegalin1029@gmail.com>
2025-04-09 15:19:14 +0800

    Update index.js

--- index.js
+++ index.js
@@ -4,6 +4,7 @@ const fs = require('fs');
 const path = require('path');
 const app = express();
 const PORT = process.env.PORT || 3000;
+app.use(express.static(__dirname));
 
 app.use(express.json());
 
